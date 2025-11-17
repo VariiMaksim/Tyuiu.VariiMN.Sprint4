78f2d969b4b956c035a8e76680081edea1b1a870
Tyuiu.VariiMN@outlook.com <maks.varii@mail.ru>
2025-11-17 19:08:44 +0500

    Task4

---
+++ Tyuiu.VariiMN.Sprint4.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.VariiMN.Sprint4.Task4.V10.Lib;
+
+namespace Tyuiu.VariiMN.Sprint4.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { 6, 6, 5, 3, 3 },
+                                          { 5, 7, 4, 6, 4 },
+                                          { 1, 2, 4, 1, 5 },
+                                          { 1, 7, 2, 5, 7 },
+                                          { 4, 2, 6, 5, 6 } };
+            int[,] res = ds.Calculate(mas2);
+            int[,] wait = new int[5, 5] { { 6, 6, 0, 0, 0 },
+                                         { 0, 0, 4, 6, 4 },
+                                         { 0, 2, 4, 0, 0 },
+                                         { 0, 0, 2, 0, 0 },
+                                         { 4, 2, 6, 0, 6 } };
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Assert.AreEqual(wait[i, j], res[i, j]);
+                }
+
+            }
+        }
+    }
+}
---
+++ Tyuiu.VariiMN.Sprint4.Task4.V10/Program.cs
@@ -0,0 +1,81 @@
+﻿using Tyuiu.VariiMN.Sprint4.Task4.V10.Lib;
+
+namespace Tyuiu.VariiMN.Sprint4.Task4.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Варий М. Н. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Варий Максим Николаевич  | ИСПб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов,                    *");
+            Console.WriteLine("* заполненный значениями с клавиатуры в диапазоне от 1 до 7.              *");
+            Console.WriteLine("* Заменить нечётные элементы массива на 0.                                 *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] array = new int[5, 5];
+            Console.WriteLine("Введите 25 целых чисел в диапазоне от 1 до 7:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"Элемент [{i + 1},{j + 1}]: ");
+                    string input = Console.ReadLine();
+                    int value = int.Parse(input);
+
+                    if (value < 2 || value > 8)
+                    {
+                        Console.WriteLine("!Вне dungeon");
+                        j--; 
+                        continue;
+                    }
+
+                    array[i, j] = value;
+                }
+            }
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(array[i, j]);
+                    if (j < 4) Console.Write(", ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] modifiedArray = ds.Calculate(array);
+
+            Console.WriteLine("Массив после замены нечётных на 0:");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write(modifiedArray[i, j]);
+                    if (j < 4) Console.Write(", ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
