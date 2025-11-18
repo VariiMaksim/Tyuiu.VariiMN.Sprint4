e0cf61283bb56eabc15a2892cbd03f689d38761c
Tyuiu.VariiMN@outlook.com <maks.varii@mail.ru>
2025-11-18 12:59:19 +0500

    Task6

---
+++ Tyuiu.VariiMN.Sprint4.Task6.V19.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.VariiMN.Sprint4.Task6.V19.Lib;
+
+namespace Tyuiu.VariiMN.Sprint4.Task6.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var week = new string[] { "Chrome", "Firefox", "Safari", "Opera", "Edge", "Internet Explorer", "Brave" };
+            int res = ds.Calculate(week);
+            int wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VariiMN.Sprint4.Task6.V19/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.VariiMN.Sprint4.Task6.V19.Lib;
+
+namespace Tyuiu.VariiMN.Sprint4.Task6.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Варий М. Н. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Варий Максим Николаевич  | ИСПб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных используя класс Array подсчитайте           *");
+            Console.WriteLine("* количество элементов, длина которых больше 5.                           *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            var week = new string[] { "Chrome", "Firefox", "Safari", "Opera", "Edge", "Internet Explorer", "Brave" };
+
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i <= week.Length - 1; i++)
+            {
+                Console.WriteLine(week[i]);
+            }
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int nums = ds.Calculate(week);
+
+            Console.WriteLine(nums);
+            Console.ReadKey();
+        }
+    }
+}
