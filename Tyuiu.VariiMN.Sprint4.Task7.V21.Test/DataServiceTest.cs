064e8c65512d4a13cb6088869f5faaad3d905342
Tyuiu.VariiMN@outlook.com <maks.varii@mail.ru>
2025-11-18 17:04:42 +0500

    Task7

---
+++ Tyuiu.VariiMN.Sprint4.Task7.V21.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.Common;
+using Tyuiu.VariiMN.Sprint4.Task7.V21.Lib;
+
+namespace Tyuiu.VariiMN.Sprint4.Task7.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int rows = 4;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "425963128528";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 32;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.VariiMN.Sprint4.Task7.V21/Program.cs
@@ -0,0 +1,60 @@
+﻿using Tyuiu.VariiMN.Sprint4.Task7.V21.Lib;
+
+namespace Tyuiu.VariiMN.Sprint4.Task7.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Варий М. Н. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Варий Максим Николаевич  | ИСПб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных используя класс Array подсчитайте           *");
+            Console.WriteLine("* количество элементов, длина которых больше 5.                           *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int rows = 4;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+
+            string str = "425963128528";
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(rows, columns, str);
+
+            Console.WriteLine("Сумма четных чисел:" + res);
+            Console.ReadKey();
+        }
+    }
+}
